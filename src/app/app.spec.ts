0277c811d349bb1f46fb537e3f276536fe1d5ab7
vinod salunke <salunkevinod45@gmail.com>
2026-04-01 12:40:49 +0530

    fix testing db

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-cicd-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Vinod angular-cicd-app');
   });
 });
